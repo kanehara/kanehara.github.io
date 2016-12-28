336d3d31885a67aa856602136a909bb0cbba247f
yohei.kanehara <yohei.kanehara@vodori.com>
2016-12-28 15:22:42 -0500

    Change to index.php to html

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=10; IE=edge; chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="styling/abra.css">
-    <link rel="stylesheet" type="text/css" href="styling/css/main.css">
-    <!-- Google Font - Cabin -->
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,700,300' rel='stylesheet' type='text/css'>
-    <title>Yohei Kanehara</title>
-</head>
+
