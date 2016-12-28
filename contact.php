ab235943274c0d33136fbbdcc6554aa03a8ccc50
yohei.kanehara <yohei.kanehara@vodori.com>
2016-12-28 14:47:37 -0500

    Cloned yoheikanehara.com

---
+++ contact.php
@@ -0,0 +1,24 @@
+<div class="blueDownArrow"></div>
+<div class="container">
+    <h2 class="abraAppear">Contact</h2>
+    <hr class="abraAppearHr"/>
+    <div class="row contactRow">
+      <div class="col-md-4"></div>
+      <div class="col-md-4">
+          <span class="abraTypeWrap">
+            <a href="mailto:kanehara@umich.edu">
+                <i class="fa fa-envelope-o abraAppear"></i>
+                <span  id="contactEmail" class="abraType">kanehara@umich.edu</span><br/><br/>
+            </a>
+            <a href="https://www.linkedin.com/in/yohei-kanehara-5b492670" target="_blank">
+                <i class="fa fa-linkedin abraAppear"></i>
+                <span id="contactLinkedIn" class="abraType">LinkedIn</span><br/><br/>
+            </a>
+            <i class="fa fa-skype abraAppear"></i>
+            <span id="contactSkype" class="abraType">kanehara.yohei</span><br/><br/>
+          </span>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+    <hr class="abraAppearHr"/>
+</div>
---
+++ home.php
@@ -0,0 +1,14 @@
+<div class="overlay"></div>
+<div class="container">
+    <h1 class="abraAppear"><strong>Yohei Kanehara</strong></h1>
+    <hr class="abraAppearHr" style="width:50%; text-align:center;"/>
+    <span class="abraTypeWrap">
+        <h3 id="homeTitle" class="abraType">Software Developer</h3>
+        <h3 id="homeEmail" class="abraType">kanehara@umich.edu</h3>
+    </span>
+</div>
+<div class="scrolldown">
+    <a href="#Profile">
+        <span class="glyphicon glyphicon-chevron-down"></span>
+    </a>
+</div>
